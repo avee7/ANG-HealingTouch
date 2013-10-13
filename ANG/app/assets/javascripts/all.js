994d9cf42fd965f3320d1fe24f73caa8c9e70b0d
Avee <avinash.kaur90@gmail.com>
2013-10-13 16:37:20 +0530

    initial setup

---
+++ ANG/app/assets/javascripts/all.js
@@ -0,0 +1,13 @@
+/* ...
+*= require application
+*= require jquery
+*= require bootstrap
+*= require filter
+*= require jquery.carouFredSel-6.1.0-packed 
+*= require jquery.flexslider-min
+*= require jquery.isotope 
+*= require jquery.prettyPhoto
+*= require jquery.tweet
+*= require custom
+
+*/
---
+++ ANG/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ANG::Application.config.secret_token = 'c1109d77432b85e97d0b511aca3269f6fe619c7f5e7a3e9bb74d75bb29f8ff5a87f08e715cad12266a4c5d6e1df6efb3dfb4d789ac9c923e653ffe7247c46474'
